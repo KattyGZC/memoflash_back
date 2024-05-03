bdc666afc0a1e38f2777ab5f515b4191e6ab46e5
KattyGZC <kattyko.gzc@gmail.com>
2024-05-03 14:18:23 -0500

    Creación del modelo de datos

--- memo_cards/admin.py
+++ memo_cards/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Topic, Card, Item
 
-# Register your models here.
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Card)
+class CardAdmin(admin.ModelAdmin):
+    list_display = ('id', 'created_at', 'modified_at')
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content')
--- memo_cards/models.py
+++ memo_cards/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Topic(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='topics', verbose_name='User')
+    name = models.CharField('Name', max_length=255)
+    enable = models.BooleanField('Enable', default=False)
+
+    class Meta:
+        verbose_name = 'Topic'
+        verbose_name_plural = 'Topics'
+
+    def __str__(self):
+        return self.name 
+    
+class Card(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name="cards", verbose_name='Topic')
+    created_at = models.DateTimeField('Created', auto_now_add=True)
+    modified_at = models.DateTimeField('Modified', auto_now=True)
+
+    class Meta:
+        verbose_name = 'Card'
+        verbose_name_plural = 'Cards'
+
+
+class Item(models.Model):
+    card = models.ForeignKey(Card, on_delete=models.CASCADE, related_name='items', verbose_name='Card')
+    title = models.CharField('Title', max_length=255)
+    content = models.TextField('Content')
+    comment = models.TextField('Comment')
+
+    class Meta:
+        verbose_name = 'Item'
+        verbose_name_plural = 'Items'
