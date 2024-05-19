c9b5f024e5c987aa60682d1e8121e421861fdf3f
KattyGZC <kattyko.gzc@gmail.com>
2024-05-19 03:59:14 -0500

    Vamos a intentar con esto

--- start.sh
+++ start.sh
@@ -8,6 +8,8 @@ set -a
 source "$ENV_PATH"
 set +a
 
+source ${ENV_GUNICORN_CMD}/activate
+
 # Continuar con la instalación de requisitos y ejecución de supervisord
 pip install -r ../memoflash_back/requirements.txt
 ${ENV_GUNICORN_CMD}/supervisord -c ${ENV_GUNICORN_DIR}/supervisord.conf
