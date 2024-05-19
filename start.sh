e49853a8d13dab6c2cd297cecc1392e68d4533ae
KattyGZC <kattyko.gzc@gmail.com>
2024-05-18 21:33:21 -0500

    Manejo de ejecución usando supervisor

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Asume que el primer argumento es la ruta al archivo .env
+ENV_PATH=$1
+
+
+# Carga las variables de entorno desde el archivo .env
+set -a
+source "$ENV_PATH"
+set +a
+
+# Continuar con la instalación de requisitos y ejecución de supervisord
+pip install -r ../memoflash_back/requirements.txt
+${ENV_GUNICORN_CMD}/supervisord -c ${ENV_GUNICORN_DIR}/supervisord.conf
+
