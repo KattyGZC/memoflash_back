c7fba706b7f374cab8cfdefe5b927a756e4b8b5a
KattyGZC <kattyko.gzc@gmail.com>
2024-07-21 17:58:24 -0500

    Configuracions faltantes

--- memoflash_back/settings.py
+++ memoflash_back/settings.py
@@ -11,11 +11,16 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 import os
+import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+env = environ.Env()
 
+# Leer el archivo .env
+env_file = BASE_DIR / '.env'
+environ.Env.read_env(env_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
